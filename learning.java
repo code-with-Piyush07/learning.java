d681a9fda79c1bc664cba26ddcba57e9869832bc
Piyush <singh2214piyush@gmail.com>
2026-08-27 16:42:20 +0530

    electricity bill calculation

--- learning.java
+++ learning.java
@@ -1,8 +1,20 @@
 class learning {
     public static void main(String[] args) {
-        int a=1 ,b=2;
-        int result= (a+b);
-        System.out.println(result);
+        double unit = 1200;
+        if(unit>=1000){
+            double result = 500+ (unit - 1000)*5 ;
+            double surcharge = result + 0.1*result;
+            System.out.println("bill = " + surcharge);
+        }
+        else if (unit>=200 && unit<1000){
+            double billprice = 500 + (unit - 200)*5;
+           System.out.println(billprice);
     }
-    
+       else if(unit>=100&& unit<200){
+        double amount = 200 + (unit - 100)*3;
+        System.out.println(amount);
+       }
+       else
+        System.out.println((100 - unit) *2);
+}
 }
