496a1102ba303b4aed9107e8d9e981598badfc2b
Piyush <singh2214piyush@gmail.com>
2026-09-06 16:59:29 +0530

    array of students

--- learning.java
+++ learning.java
@@ -1,22 +1,29 @@
+class students{
+    int rollno ;
+    String name;
+    int marks;
+
+
+}
 class learning{
     public static void main(String[] args) {
-        int nums [][] = new int [3][];
-         nums[0] = new int [3];
-         nums[1] = new int [2];
-         nums[2] = new int[4];
-        for(int i = 0; i<nums.length;i++){
-            for(int j = 0;j<nums.length;j++){
-                nums[i][j]= (int)(Math.random()*10);
-            }
-      
-        }
-        for(int n [] : nums)
-        {
-            for(int m:n )
-        {
-    System.out.print(m + " ");
-    }
-    System.out.println();
-    }
+        students s1 = new students();
+        s1.rollno =4;
+        s1.name = "Piyush";
+        s1.marks = 90;
+
+        students s2 = new students();
+        s2.rollno= 5;
+        s2.name = "god";
+        s2.marks = 100;
+
+       students student[]= new students[2];
+       student [0]= s1;
+       student[1]= s2;
+
+       for(int i = 0;i<student.length;i++){
+        System.out.println(student[i].name + ":" + student[i].marks);
+
+       }
     }
 }
